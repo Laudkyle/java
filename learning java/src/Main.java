a1086c7ff527f0994bb003cc0498b18a7e452eb4
Matilda-Oppong <matildaoppong275@gmail.com>
2023-03-26 16:06:55 +0000

    tenary operators

---
+++ learning java/src/Main.java
@@ -0,0 +1,28 @@
+import java.lang.ref.Reference;
+
+public class Main {
+    public static void main(String[] args) {
+//        This is for integers
+        byte thebyte =-128;
+        short theshort = 9899;
+        int theint = 334;
+        long thelong = 8879879879879l;
+//        This is for float
+        float thefloat = 45.909F;
+        double thedouble = 489834.078787897478;
+
+        boolean bool =true;
+        char character = 'A';
+
+//        Reference datatypes
+    String name = new String("Kyle");
+        System.out.println(name);
+        name = name.toUpperCase();
+        System.out.println(name);
+//        Teneray operators
+        String message = name == "kyle"?
+                "I am kyle":
+                "I am KYLE";
+        System.out.println(message);
+    }
+}
